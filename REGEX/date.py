582d29916ec8646c5f99bf0f406059fa80c35a0f
n3tsploit <netsploit@tutanota.com>
2022-06-10 02:33:54 +0300

    CHAPTER 7

--- REGEX/date.py
+++ REGEX/date.py
@@ -12,5 +12,5 @@ print(found)
 """
 Write a regular expression that can detect dates in the DD/MM/YYYY format. Assume that the days range
 from 01 to 31, the months range from 01 to 12, and the years range from 1000 to 2999. Note that if the day
-or month is a single digit, it’ll have a leading zero.
+or month   is a single digit, it’ll have a leading zero.
 """
