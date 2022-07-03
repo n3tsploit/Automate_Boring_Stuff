0385c6d5d21144cb3fff54c190b0c3c6a55e9e29
n3tsploit <netsploit@tutanota.com>
2022-07-03 07:36:54 +0300

    CHAPTER 16

---
+++ WORKING WITH CSV FILES AND JSON DATA/FetchingCurrentWeatherData.py
@@ -0,0 +1,30 @@
+import json
+import os
+
+import requests
+from dotenv import load_dotenv
+load_dotenv()
+import sys
+
+if len(sys.argv) < 2:
+    print('USE: python3 FetchingCurrentWeatherData.py cityname, country abbreviation i.e KE for kenya')
+else:
+    location = ''.join(sys.argv[1:]).strip()
+    api_key = os.environ.get('weather_key')
+    url = f'https://api.openweathermap.org/data/2.5/forecast/daily?q={location}&cnt=3&APPID={api_key}'
+    content = requests.get(url)
+    content.raise_for_status()
+    print(content.text)
+    data = json.loads(content.text)
+
+
+"""
+Checking the weather seems fairly trivial: Open your web browser, click
+the address bar, type the URL to a weather website (or search for one and
+then click the link), wait for the page to load, look past all the ads, and so
+on.
+Actually, there are a lot of boring steps you could skip if you had a
+program that downloaded the weather forecast for the next few days and
+printed it as plaintext. This program uses the requests module from
+Chapter 12 to download data from the web.
+"""
